13d9c89cb68d945f8270bbdccda70132214b409a
tim.kukom@gmail.com <Puzyako@50202.local>
2017-03-15 11:31:18 +0000

    Корзина

--- include/main/soc.php
+++ include/main/soc.php
@@ -1,3 +1,4 @@
 <li><a href="https://vk.com/kupibilet.online"><i class="engIcon setIcon-15 setIcon-vk"></i>Вконтакте</a></li>
-<li><a href="https://www.instagram.com/kupibilet.online"><i class="engIcon setIcon-15 setIcon-in"></i>Instagram</i></a></li>
+<li><a href="https://www.instagram.com/kupibilet.online"><i class="engIcon setIcon-15 setIcon-in"></i>Instagram</a></li>
 <li><a href="https://www.facebook.com/pg/КупиБилет-online-969873639814627/?fref=ts"><i class="engIcon setIcon-15 setIcon-fb"></i>Facebook</a></li>
+<li><a href="https://ok.ru/group/53191506722965"><i class="engIcon setIcon-15 setIcon-od"></i>Одноклассники</a> </li>
