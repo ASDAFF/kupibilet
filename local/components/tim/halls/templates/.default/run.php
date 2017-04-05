dd6ecf3719917f90e71780912a4b2a767ce9a629
User A0075956 <a0075956@gange.from.sh>
2017-04-05 13:56:09 +0300

    Верстка

--- local/components/tim/halls/templates/.default/run.php
+++ local/components/tim/halls/templates/.default/run.php
@@ -37,8 +37,7 @@ $sits = \Local\Sale\Cart::getSitsByRun($run['ID']);
 $reserved = \Local\Sale\Reserve::getByRun($run['ID'])
 
 
-?>
-	<div class="engBox">
+?>	<div class="engBox">
 		<div class="elZal-top">
 
 			<div class="engRow">
@@ -54,20 +53,24 @@ $reserved = \Local\Sale\Reserve::getByRun($run['ID'])
 							<b  class="engBox-4  engMb-4"><?= $time ?></b>
 							<span  class="engBox-7 engMb-6"><?= $hall['NAME'] ?></span>
 						</div>
-						<div class=" engBox-3 engPl-3 engMb-12">
-							<div class="it-cart">
-								<div class="it-cart-icon">
-									<svg class="engSvg set-45" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-										<path d="M 16 3.09375 L 15.28125 3.8125 L 7.09375 12 L 3 12 L 2 12 L 2 13 L 2 17 L 2 18 L 3 18 L 3.25 18 L 6.03125 27.28125 L 6.25 28 L 7 28 L 25 28 L 25.75 28 L 25.96875 27.28125 L 28.75 18 L 29 18 L 30 18 L 30 17 L 30 13 L 30 12 L 29 12 L 24.90625 12 L 16.71875 3.8125 L 16 3.09375 z M 16 5.9375 L 22.0625 12 L 9.9375 12 L 16 5.9375 z M 4 14 L 28 14 L 28 16 L 27.25 16 L 27.03125 16.71875 L 24.25 26 L 7.75 26 L 4.96875 16.71875 L 4.75 16 L 4 16 L 4 14 z M 11 17 L 11 24 L 13 24 L 13 17 L 11 17 z M 15 17 L 15 24 L 17 24 L 17 17 L 15 17 z M 19 17 L 19 24 L 21 24 L 21 17 L 19 17 z"></path>
-									</svg>
-									<i class="engCircle" id="current_cart_count"><?= $cartSummary['COUNT'] ?></i>
+						<div class=" engBox-3 engPl-3 engMb-12 engMb-cssText-center">
+							<?if($_GET['test'] == 'test'):?>
+								<a href="/personal/cart/" class="it-cart-btn">Перейти в корзину</a>
+							<?else:?>
+								<div class="it-cart">
+									<div class="it-cart-icon">
+										<svg class="engSvg set-45" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+											<path d="M 16 3.09375 L 15.28125 3.8125 L 7.09375 12 L 3 12 L 2 12 L 2 13 L 2 17 L 2 18 L 3 18 L 3.25 18 L 6.03125 27.28125 L 6.25 28 L 7 28 L 25 28 L 25.75 28 L 25.96875 27.28125 L 28.75 18 L 29 18 L 30 18 L 30 17 L 30 13 L 30 12 L 29 12 L 24.90625 12 L 16.71875 3.8125 L 16 3.09375 z M 16 5.9375 L 22.0625 12 L 9.9375 12 L 16 5.9375 z M 4 14 L 28 14 L 28 16 L 27.25 16 L 27.03125 16.71875 L 24.25 26 L 7.75 26 L 4.96875 16.71875 L 4.75 16 L 4 16 L 4 14 z M 11 17 L 11 24 L 13 24 L 13 17 L 11 17 z M 15 17 L 15 24 L 17 24 L 17 17 L 15 17 z M 19 17 L 19 24 L 21 24 L 21 17 L 19 17 z"></path>
+										</svg>
+										<i class="engCircle" id="current_cart_count"><?= $cartSummary['COUNT'] ?></i>
+									</div>
+									<div class="it-cart-title">Ваша корзина</div>
+									<div class="it-cart-inf">
+										<a href="/personal/cart/" title="Перейти в корзину">
+											Сумма: <span id="current_cart_price"><?= $cartSummary['PRICE'] ?></span> руб.</a>
+									</div>
 								</div>
-								<div class="it-cart-title">Ваша корзина</div>
-								<div class="it-cart-inf">
-									<a href="/personal/cart/" title="Перейти в корзину">
-										Сумма: <span id="current_cart_price"><?= $cartSummary['PRICE'] ?></span> руб.</a>
-								</div>
-							</div>
+							<?endIf;?>
 						</div>
 					</div>
 				</div>
